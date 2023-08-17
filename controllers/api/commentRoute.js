47690beb136342af8e84f8dc1a2bd078a2b3d036
EliteHuskie <stratton_10@hotmail.com>
2023-08-17 19:46:19 -0300

    Adding modifications and files for the comment route

---
+++ controllers/api/commentRoute.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const withAuth = require('../../utils/auth');
+const { Comment } = require('../../models');
+
+// Route to create a new comment
+router.post('/create-comment', withAuth, async (req, res) => {
+  try {
+    // Create a new comment in the database
+    const newComment = await Comment.create({
+      content: req.body.comment,
+      user_id: req.session.user_id,
+      post_id: req.body.post_id,
+      date_created: new Date().toISOString(), 
+    });
+
+    res.status(200).json(newComment);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 
 const userRoutes = require('./userRoute');
-const postRoutes = require('./postRoute'); // Adjust the path based on your project structure
+const postRoutes = require('./postRoute');
+const commentRoutes = require('./commentRoute'); // Import comment routes
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes); // Use the correct path for comments
 
 module.exports = router;

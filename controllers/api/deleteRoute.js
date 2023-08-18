e1b56102408c87613fbe469d6ac13995f036b211
EliteHuskie <stratton_10@hotmail.com>
2023-08-17 23:38:31 -0300

    Adding delete route and router in controllers api

---
+++ controllers/api/deleteRoute.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+const withAuth = require('../../utils/auth');
+const { Post } = require('../../models');
+
+// Route to delete a post
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    // Find the post by ID
+    const post = await Post.findByPk(postId);
+
+    if (!post) {
+      res.status(404).json({ message: 'Post not found.' });
+      return;
+    }
+
+    // Ensure that only the post owner can delete the post
+    if (post.user_id !== req.session.user_id) {
+      res.status(403).json({ message: 'You are not authorized to delete this post.' });
+      return;
+    }
+
+    // Delete the post
+    await post.destroy();
+
+    res.status(204).send(); // No content after successful deletion
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
 
 const userRoutes = require('./userRoute');
-const postRoutes = require('./postRoute'); // Adjust the path based on your project structure
+const postRoutes = require('./postRoute');
+const commentRoutes = require('./commentRoute');
+const deleteRoutes = require('./deleteRoute'); 
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
+router.use('/delete', deleteRoutes);
 
 module.exports = router;
